d0efd734ab9c30c91abd8cdaaedd358b8728872f
NoumanEjaz-dev <noumanejaz488@gamil.com>
2026-03-03 05:42:34 +0500

    added the html frontend and controllers

--- src/main/java/com/Blog/Blog_project/BlogController/BlogController.java
+++ src/main/java/com/Blog/Blog_project/BlogController/BlogController.java
@@ -1,10 +1,13 @@
 package com.Blog.Blog_project.BlogController;
 
 import com.Blog.Blog_project.BlogRepo.BlogRepo;
+import com.Blog.Blog_project.model.BlogPost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -15,7 +18,20 @@ public class BlogController {
 
     @GetMapping("/")
     public String viewHomePage( Model model){
-    model.addAttribute("postRepo", postRepo.findAll());
+    model.addAttribute("posts", postRepo.findAll());
     return "index";
     }
+
+    @GetMapping("/new")
+    public String newPost(Model model){
+        model.addAttribute("post", new BlogPost());
+        return "new_post";
+    }
+
+    @PostMapping("/save")
+    public String savePost(@ModelAttribute("post") BlogPost post){
+        postRepo.save(post);
+        return "redirect:/";
+    }
 }
+
